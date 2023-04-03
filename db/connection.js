4e830f12e5e9c2f910169a8b0b4aa9caee0364af
saraschroeder <sara.schroeder00@gmail.com>
2023-04-02 19:04:58 -0500

    re configuring index.js file:

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const connect = mysql.createConnection({
   host: "127.0.0.1",
   user:"root",
   password: "Wrenley1!",
-  database: "employee_db",
+  database: "employees_db",
 });
 
 connect.connect((err) => {
